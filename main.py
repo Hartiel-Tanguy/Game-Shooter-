800b7ba27c65ef6e69b895d4774ca3e56d892ead
Antoine <tanguy.hartiel.1@gmail.com>
2022-08-16 10:53:00 +0200

    placement du joueur

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import pygame
 pygame.init()
 
+#creations du joueur 
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load("assets/player.png")
+        self.rect = self.image.get_rect()
+
 #generé la fenetre du jeux 
 
 pygame.display.set_caption("game shooter")
@@ -9,12 +21,22 @@ screen = pygame.display.set_mode((1080,720))
 #importé de chargé l'arriere plant de notre jeux 
 background = pygame.image.load('assets/bg.jpg')
 
+
+
+#chargé le joueur
+
+player = Player()
+
 running = True
 #boucle tanr que cette condision est maintenue vrai
 while running:
 
     #applique l'arriere plan 
     screen.blit(background, (0,-200))
+
+
+    #applique le joueur
+    screen.blit(player.image, player.rect)
 
     #metre a jour l'ecran
     pygame.display.flip()
